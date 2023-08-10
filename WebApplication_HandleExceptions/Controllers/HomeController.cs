b5727c51285d6bf1eb9a61b15cb48cab82819c07
Ismail Aydemir <theangelo112@gmail.com>
2023-08-10 17:06:11 +0300

    Add project files.

---
+++ WebApplication_HandleExceptions/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApplication_HandleExceptions.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        // localhost:[port]/error-development
+        // action seviyesinde bir dependency injection
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices]IHostEnvironment host)
+        {
+
+        }
+    }
+}
---
+++ WebApplication_HandleExceptions/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_HandleExceptions.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            int zero = 0;
+            decimal result = 10 / zero;
+            return Ok();
+        }
+    }
+}
